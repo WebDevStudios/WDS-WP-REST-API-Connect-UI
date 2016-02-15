1abb888c95b592799d2a691f351af7d1c4dc2c30
Justin Sternberg <justin@dsgnwrks.pro>
2016-02-15 14:35:09 -0500

    Add compress/zip grunt job for generating a complete plugin zip (and generate the language .pot file)

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,15 +72,61 @@ module.exports = function( grunt ) {
 					}
 				}
 			}
-		}
+		},
 
+		// Command line commands
+		exec: {
+			makezip: 'echo "Updating .zip file. Please hold..." && grunt compress',
+		},
+
+		// make a zipfile
+		compress: {
+			main: {
+				options: {
+					mode: 'zip',
+					archive: 'wds-rest-connect-ui.zip'
+				},
+				files: [ {
+						expand: true,
+						// cwd: '/',
+						src: [
+							'**',
+							'!**/**dandelion**.yml',
+							'!**/phpunit.xml',
+							'!**/package.json',
+							'!**/node_modules/**',
+							'!**/bin/**',
+							'!**/tests/**',
+							'!**/sass/**',
+							'!**.zip',
+							'!**/**.orig',
+							'!**/**.map',
+							'!**/**Gruntfile.js',
+							'!vendor/league/oauth1-client/resources/**',
+							'!vendor/rmccue/requests/docs/**',
+							'!vendor/rmccue/requests/examples/**',
+							'!vendor/symfony/event-dispatcher/Tests/**',
+							'!vendor/guzzle/guzzle/phing/**',
+							'!vendor/guzzle/guzzle/docs/**'
+						],
+						dest: '/'
+				} ]
+			}
+		},
+
+		githooks: {
+			all: {
+				// create zip and deploy changes to ftp
+				'pre-push': 'compress exec'
+			}
+		}
 	} );
 
 	// Default task.
 	grunt.registerTask( 'scripts', [] );
 	grunt.registerTask( 'styles', [] );
 	grunt.registerTask( 'php', [ 'addtextdomain', 'makepot' ] );
-	grunt.registerTask( 'default', ['styles', 'scripts', 'php'] );
+	grunt.registerTask( 'default', ['styles', 'scripts', 'php', 'compress'] );
 
 	grunt.util.linefeed = '\n';
 };
